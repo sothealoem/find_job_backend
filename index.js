d5e0e5a1c3f8c79f8a486a5a9f9ba7d2b4b1a56f
sothealoem <loemsothea222@gmail.com>
2024-05-26 17:18:50 +0700

    update_1

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ mongoose
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api/jobs", jobRouter);
+
 app.listen(process.env.PORT || port, () =>
   console.log(`The Hub is listening on port ${process.env.PORT}!`)
 );
